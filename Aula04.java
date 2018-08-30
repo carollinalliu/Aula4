cfb868a3e67b1c3440707e7c81a6a1c7072329c3
carollinalliu <42851748+carollinalliu@users.noreply.github.com>
2018-08-30 20:10:57 -0300

    Add files via upload

---
+++ Aula04.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula.pkg04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lab208
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner Leia = new Scanner (System.in);
+        System.out.println("Digite um mês");
+        int opcao = Leia.nextInt();
+        if (opcao <=12);
+        switch (opcao){
+            case 1:
+                System.out.println("Janeiro");
+          
+                 case 2:
+                System.out.println("Fvereiro");
+
+                 case 3:
+                System.out.println("Março");
+
+                 case 4:
+                System.out.println("Abril");
+
+                 case 5:
+                System.out.println("Maio");
+
+                 case 6:
+                System.out.println("Junho");
+                
+                 case 7:
+                System.out.println("Julho");
+            
+                 case 8:
+                System.out.println("Agosto");
+                
+                 case 9:
+                System.out.println("Setembro");
+
+                 case 10:
+                System.out.println("Outubro");
+
+                 case 11:
+                System.out.println("Novembro");
+
+                 case 12:
+                System.out.println("Dezembro");
+            
+                
+                
+                    
+        }
+        
+        
+    }
+    
+}
